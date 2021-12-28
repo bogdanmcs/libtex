7565a0b4fdf7af766145a66b4b5f77dfff4f2ddd
bogdanmcs <62352065+bogdanmcs@users.noreply.github.com>
2021-12-28 18:18:13 +0200

    feat(admin): view all books

---
+++ app/src/main/java/com/ad_victoriam/libtex/model/Book.java
@@ -0,0 +1,17 @@
+package com.ad_victoriam.libtex.model;
+
+public class Book {
+    private String name;
+
+    public Book() {
+        // data snapshot
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
