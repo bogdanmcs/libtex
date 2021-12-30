e9669b24af5815d09072ec75920454bfcbc918f4
bogdanmcs <62352065+bogdanmcs@users.noreply.github.com>
2021-12-31 01:08:47 +0200

    feat(user): improved ui for login and register

--- app/src/main/java/com/ad_victoriam/libtex/auth/LoginActivity.java
+++ app/src/main/java/com/ad_victoriam/libtex/auth/LoginActivity.java
@@ -3,8 +3,9 @@ package com.ad_victoriam.libtex.auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,6 +17,7 @@ import com.ad_victoriam.libtex.user.UserHomeActivity;
 import com.ad_victoriam.libtex.user.auth.RegisterActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,44 +26,77 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-    private EditText editTextEmail;
-    private EditText editTextPassword;
+    private TextInputEditText eEmail;
+    private TextInputEditText ePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //
         mAuth = FirebaseAuth.getInstance();
 
-//        final FirebaseDatabase database = FirebaseDatabase.getInstance("https://libtex-a007e-default-rtdb.europe-west1.firebasedatabase.app/");
-//        DatabaseReference databaseReference = database.getReference();
+        eEmail = findViewById(R.id.eEmail);
+        ePassword = findViewById(R.id.ePassword);
 
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
+        final Button bLogIn = findViewById(R.id.bLogIn);
+        final Button bCreateAccount = findViewById(R.id.bCreateAccount);
+        bLogIn.setOnClickListener(this::validateCredentials);
+        bCreateAccount.setOnClickListener(this::bCreateAccount);
+    }
+
+    private void bCreateAccount(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    private void validateCredentials(View view) {
+        String email = eEmail.getText().toString();
+        String password = ePassword.getText().toString();
+
+        boolean errorFlag = false;
+
+        if (password.isEmpty()) {
+            ePassword.setError("Please fill this field");
+            ePassword.requestFocus();
+            errorFlag = true;
+        } else if (password.length() > 50) {
+            ePassword.setError("Maximum length is 50 characters");
+            ePassword.requestFocus();
+            errorFlag = true;
+        }
+
+        if (email.isEmpty()) {
+            eEmail.setError("Please fill this field");
+            eEmail.requestFocus();
+            errorFlag = true;
+        } else if (email.length() > 50) {
+            eEmail.setError("Maximum length is 50 characters");
+            eEmail.requestFocus();
+            errorFlag = true;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            eEmail.setError("Email is not valid");
+            eEmail.requestFocus();
+            errorFlag = true;
+        }
+
+        if (errorFlag) {
+            return;
+        }
+
+        signIn(email, password);
     }
 
     public void signIn(String email, String password) {
-        System.out.println("Trying to log in with creds:");
-        System.out.println("Email: (" + email + ")");
-        System.out.println("Password: (" + password + ")");
-
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("test123", "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("test123", "signInWithEmail:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
                         }
                     }
                 });
@@ -74,33 +109,5 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this, UserHomeActivity.class));
         }
         finish();
-    }
-
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.bLogIn:
-                String email = editTextEmail.getText().toString();
-                String password = editTextPassword.getText().toString();
-
-                if (email.isEmpty()) {
-                    editTextEmail.setError("cannot be empty!");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-
-                if (password.isEmpty()) {
-                    editTextPassword.setError("cannot be empty!");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-
-                signIn(email, password);
-                break;
-            case R.id.bRegister:
-                startActivity(new Intent(this, RegisterActivity.class));
-                break;
-
-            default:
-        }
     }
 }
--- app/src/main/java/com/ad_victoriam/libtex/user/auth/RegisterActivity.java
+++ app/src/main/java/com/ad_victoriam/libtex/user/auth/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.ad_victoriam.libtex.user.auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,83 +17,98 @@ import com.ad_victoriam.libtex.auth.LoginActivity;
 import com.ad_victoriam.libtex.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-    private EditText editTextEmail;
-    private EditText editTextPassword;
+    private TextInputEditText eEmail;
+    private TextInputEditText ePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
 
-        //
         mAuth = FirebaseAuth.getInstance();
 
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
+        eEmail = findViewById(R.id.eEmail);
+        ePassword = findViewById(R.id.ePassword);
+
+        final Button bRegister = findViewById(R.id.bRegister);
+        bRegister.setOnClickListener(this::validateCredentials);
     }
 
-    private void register(String email, String password) {
+    private void createAccount(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        // must check if email hasnt been already used
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("user-register", "createUserWithEmail:success");
                             FirebaseUser firebaseUser = mAuth.getCurrentUser();
                             storeDataIntoDatabase(firebaseUser);
+                            mAuth.signOut();
                             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                             finish();
 
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("user-register", "createUserWithEmail:failure", task.getException());
                             Toast.makeText(RegisterActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
                         }
                     }
                 });
+    }
+
+    private void validateCredentials(View view) {
+        String email = eEmail.getText().toString();
+        String password = ePassword.getText().toString();
+
+        boolean errorFlag = false;
+
+        if (password.isEmpty()) {
+            ePassword.setError("Please fill this field");
+            ePassword.requestFocus();
+            errorFlag = true;
+        } else if (password.length() > 50) {
+            ePassword.setError("Maximum length is 50 characters");
+            ePassword.requestFocus();
+            errorFlag = true;
+        }
+
+        if (email.isEmpty()) {
+            eEmail.setError("Please fill this field");
+            eEmail.requestFocus();
+            errorFlag = true;
+        } else if (email.length() > 50) {
+            eEmail.setError("Maximum length is 50 characters");
+            eEmail.requestFocus();
+            errorFlag = true;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            eEmail.setError("Email is not valid");
+            eEmail.requestFocus();
+            errorFlag = true;
+        }
+
+        if (errorFlag) {
+            return;
+        }
+
+        createAccount(email, password);
     }
 
     private void storeDataIntoDatabase(FirebaseUser firebaseUser) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://libtex-a007e-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
         User user = new User(firebaseUser.getEmail());
         databaseReference.child("users").child(firebaseUser.getUid()).setValue(user);
-    }
-
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.bRegister:
-                String email = editTextEmail.getText().toString();
-                String password = editTextPassword.getText().toString();
-
-                if (email.isEmpty()) {
-                    editTextEmail.setError("cannot be empty!");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-
-                if (password.isEmpty()) {
-                    editTextPassword.setError("cannot be empty!");
-                    editTextEmail.requestFocus();
-                    return;
-                }
-
-                register(email, password);
-                break;
-
-            default:
-        }
     }
 }
