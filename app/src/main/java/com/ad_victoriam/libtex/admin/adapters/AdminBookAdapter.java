73d098c6414a20df1fb5a6d5727fe4bf5e62f05f
bogdanmcs <62352065+bogdanmcs@users.noreply.github.com>
2022-04-21 00:46:31 +0300

    feat(admin): immediately add user loans to general loans (used for more general operations)

--- app/src/main/java/com/ad_victoriam/libtex/admin/adapters/AdminBookAdapter.java
+++ app/src/main/java/com/ad_victoriam/libtex/admin/adapters/AdminBookAdapter.java
@@ -3,12 +3,14 @@ package com.ad_victoriam.libtex.admin.adapters;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,17 +18,32 @@ import com.ad_victoriam.libtex.R;
 import com.ad_victoriam.libtex.admin.activities.books.AdminBookDetailsActivity;
 import com.ad_victoriam.libtex.admin.models.AdminBook;
 import com.ad_victoriam.libtex.admin.models.AdminLoan;
+import com.ad_victoriam.libtex.admin.models.GeneralLoan;
+import com.ad_victoriam.libtex.common.models.Reservation;
 import com.ad_victoriam.libtex.common.models.User;
+import com.ad_victoriam.libtex.common.utils.ReservationStatus;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.Transaction;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.StreamSupport;
 
 public class AdminBookAdapter extends RecyclerView.Adapter<AdminBookAdapter.BookViewHolder> {
 
+    private DatabaseReference databaseReference;
     private final Context context;
     private final List<AdminBook> adminBooks;
 
@@ -50,6 +67,9 @@ public class AdminBookAdapter extends RecyclerView.Adapter<AdminBookAdapter.Book
     @Override
     public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_admin_book, parent, false);
+        databaseReference = FirebaseDatabase
+                .getInstance("https://libtex-a007e-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference();
         return new BookViewHolder(view);
     }
 
@@ -113,43 +133,134 @@ public class AdminBookAdapter extends RecyclerView.Adapter<AdminBookAdapter.Book
     }
 
     private void assignBookToUser(View view, int position) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://libtex-a007e-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         AdminBook adminBook = adminBooks.get(position);
         databaseReference
-                .child("books")
+                .child(context.getString(R.string.n_books))
                 .child(currentUser.getUid())
                 .child(adminBook.getUid())
-                .child("availableQuantity")
+                .child(context.getString(R.string.p_book_available_quantity))
                 .setValue(adminBook.getAvailableQuantity() - 1);
 
         AdminLoan adminLoan = new AdminLoan(adminBook.getUid());
         String bookLoanKey = databaseReference
-                .child("users")
+                .child(context.getString(R.string.n_users))
                 .child(user.getUid())
-                .child("book-loans")
+                .child(context.getString(R.string.n_book_loans))
                 .push()
                 .getKey();
 
         databaseReference
-                .child("users")
+                .child(context.getString(R.string.n_users))
                 .child(user.getUid())
-                .child("book-loans")
-                .child("current-loans")
+                .child(context.getString(R.string.n_book_loans))
+                .child(context.getString(R.string.n_current_loans))
                 .child(currentUser.getUid())
                 .child(bookLoanKey)
                 .setValue(adminLoan);
 
         databaseReference
-                .child("users")
+                .child(context.getString(R.string.n_users))
                 .child(user.getUid())
-                .child("book-loans")
-                .child("loans-history")
+                .child(context.getString(R.string.n_book_loans))
+                .child(context.getString(R.string.n_loans_history))
                 .child(currentUser.getUid())
                 .child(bookLoanKey)
                 .setValue(adminLoan);
 
         Snackbar.make(view, "Book assigned successfully", Snackbar.LENGTH_SHORT).show();
+
+        addToGeneralLoans(adminBook);
+    }
+
+    private void addToGeneralLoans(AdminBook adminBook) {
+        Query query = databaseReference
+                .child(context.getString(R.string.n_general_loans))
+                .orderByChild(context.getString(R.string.p_book_title))
+                .equalTo(adminBook.getTitle());
+        query
+                .get()
+                .addOnCompleteListener(task -> {
+
+                    long generalLoansSize = StreamSupport
+                            .stream(task.getResult().getChildren().spliterator(), false)
+                            .count();
+
+                    // if nothing was found, then add new loan entry, else increase counter
+                    if (generalLoansSize == 0) {
+                        addNewLoanEntry(adminBook);
+                    } else {
+                        updateExistingLoanEntry(adminBook, task);
+                    }
+                });
+    }
+
+    private void addNewLoanEntry(AdminBook adminBook) {
+        GeneralLoan generalLoan = new GeneralLoan(
+                adminBook.getTitle(),
+                adminBook.getAuthorName(),
+                adminBook.getPublisher()
+        );
+        databaseReference
+                .child(context.getString(R.string.n_general_loans))
+                .push()
+                .setValue(generalLoan);
+    }
+
+    private boolean hasSameCoreBookDetails(AdminBook adminBook, GeneralLoan generalLoan) {
+        return adminBook.getAuthorName().equals(generalLoan.getBookAuthor()) &&
+                adminBook.getPublisher().equals(generalLoan.getBookPublisher());
+    }
+
+    private void updateExistingLoanEntry(AdminBook adminBook, Task<DataSnapshot> task) {
+        for (DataSnapshot dataSnapshot: task.getResult().getChildren()) {
+
+            GeneralLoan generalLoan = dataSnapshot.getValue(GeneralLoan.class);
+
+            if (generalLoan != null &&
+                hasSameCoreBookDetails(adminBook, generalLoan)) {
+
+                // found the entry, need to increase/update the counter
+                String generalLoanUid = dataSnapshot.getKey();
+                if (generalLoanUid != null) {
+                    increaseCounter(generalLoanUid);
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+    private void increaseCounter(String generalLoanUid) {
+        databaseReference
+                .child(context.getString(R.string.n_general_loans))
+                .child(generalLoanUid)
+                .child(context.getString(R.string.p_counter))
+                .runTransaction(new Transaction.Handler() {
+                    @NonNull
+                    @Override
+                    public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+
+                        Integer counter = currentData.getValue(Integer.class);
+
+                        if (counter == null) {
+                            return Transaction.success(currentData);
+                        }
+
+                        counter += 1;
+
+                        currentData.setValue(counter);
+                        return Transaction.success(currentData);
+                    }
+
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
+                        if (!committed) {
+                            Log.e("GENERAL_LOANS_INCREASE_COUNTER", "postTransaction:onComplete:" + error);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/ad_victoriam/libtex/admin/models/GeneralLoan.java
@@ -0,0 +1,51 @@
+package com.ad_victoriam.libtex.admin.models;
+
+public class GeneralLoan {
+
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookPublisher;
+    private int counter;
+
+    public GeneralLoan() {
+    }
+
+    public GeneralLoan(String bookTitle, String bookAuthor, String bookPublisher) {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPublisher = bookPublisher;
+        this.counter = 1;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
