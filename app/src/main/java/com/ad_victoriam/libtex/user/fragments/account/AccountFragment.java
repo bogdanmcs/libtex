0897377885dfd132d36e92cb62153bb36508613e
bogdanmcs <62352065+bogdanmcs@users.noreply.github.com>
2022-05-15 10:39:43 +0300

    fin du jeu

--- app/src/main/java/com/ad_victoriam/libtex/user/fragments/account/AccountFragment.java
+++ app/src/main/java/com/ad_victoriam/libtex/user/fragments/account/AccountFragment.java
@@ -5,23 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import com.ad_victoriam.libtex.R;
 import com.ad_victoriam.libtex.common.activities.LoginActivity;
-import com.ad_victoriam.libtex.user.models.Book;
 import com.ad_victoriam.libtex.user.utils.TopAppBar;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class AccountFragment extends Fragment {
 
